92a1052e11a8575f47ba166a14569549dab3446d
Artem <artemlutij21@gmail.com>
2026-03-10 23:09:56 +0100

    smooth animations

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,11 @@
 import "@/styles/globals.css";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  return (
+    <div key={router.pathname} className="page-fade">
+      <Component {...pageProps} />
+    </div>
+  );
 }
